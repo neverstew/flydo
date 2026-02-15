aed057116c98b23b80f78ee0b1959c53acfcb279
Matt Williams <matt@neverstew.com>
2026-02-15 15:47:23 +0000

    Remove duckdb example

--- examples/duckdb.example.ts
+++
@@ -1,7 +0,0 @@
-import { DuckDBInstance } from "@duckdb/node-api";
-
-const db = await DuckDBInstance.create(':memory:')
-const conn = await db.connect();
-
-const results = await conn.runAndRead(`select count(id) from 'https://huggingface.co/datasets/Geralt-Targaryen/books3/resolve/main/books3-00001-of-00035.parquet' limit 10`);
-console.table(results.getRows())
